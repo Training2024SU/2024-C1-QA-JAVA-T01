4030959d6c7cf2a3413e35f169f0b5c7159c6409
DanielMorales <dm34mg@gmail.com>
2024-04-09 22:08:56 -0500

    Creacion de la clase libro DAO

--- src/main/java/co/com/pinguinera/modelos/Libro.java
+++ src/main/java/co/com/pinguinera/modelos/Libro.java
@@ -1,13 +1,60 @@
 package co.com.pinguinera.modelos;
 
-public class Libro {
+import java.util.List;
+
+public class Libro implements List<Libro> {
     private int libroID;
     private String titulo;
     private String autor;
     private String areaConocimiento;
     private int numPaginas;
     private int cantEjemplares;
-    private int cantPrestados;
-    // Getters y setters
-}
 
+    public int getLibroID() {
+        return libroID;
+    }
+
+    public void setLibroID(int libroID) {
+        this.libroID = libroID;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getAreaConocimiento() {
+        return areaConocimiento;
+    }
+
+    public void setAreaConocimiento(String areaConocimiento) {
+        this.areaConocimiento = areaConocimiento;
+    }
+
+    public int getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(int numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    public int getCantEjemplares() {
+        return cantEjemplares;
+    }
+
+    public void setCantEjemplares(int cantEjemplares) {
+        this.cantEjemplares = cantEjemplares;
+    }
+}
