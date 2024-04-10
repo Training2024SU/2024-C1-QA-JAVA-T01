6615b766829b09ce3a015a97a6df7f2ab72a30fe
VasquezJulioe <vasquezjulioe@gmail.com>
2024-04-10 18:03:30 -0500

    Se agrega la clase usuario

---
+++ BibliotecaLaPinguinera/src/main/java/com/sofka/Usuario.java
@@ -0,0 +1,19 @@
+package com.sofka;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+// Clase para representar un usuario
+@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    private String nombre;
+    private String correo;
+    private String contrasena;
+    private String rol;
+
+}
