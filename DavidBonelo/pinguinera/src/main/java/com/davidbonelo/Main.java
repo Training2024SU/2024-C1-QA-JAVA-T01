2450b64798d19f62ebf52f990cf7038c55c6bef3
David Bonelo <bonelodev@gmail.com>
2024-04-15 15:56:27 -0500

    add logger for general not handled exceptions

--- DavidBonelo/pinguinera/src/main/java/com/davidbonelo/Main.java
+++ DavidBonelo/pinguinera/src/main/java/com/davidbonelo/Main.java
@@ -1,16 +1,21 @@
 package com.davidbonelo;
 
-import com.davidbonelo.persistance.UserDAO;
-import com.davidbonelo.services.UserService;
 import com.davidbonelo.ui.MainMenu;
 
 import java.sql.Connection;
+import java.util.logging.Logger;
 
 public class Main {
+    private static final Logger logger = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
         Connection connection = PinguDatabase.getConnection();
 
-        new MainMenu(connection).menu();
+        try {
+            new MainMenu(connection).menu();
+        } catch (Exception e) {
+            logger.severe("General error, please contact the developer with the following error " + "info: " + e.getMessage());
+        }
 
         PinguDatabase.close();
     }
--- DavidBonelo/pinguinera/src/main/java/com/davidbonelo/PinguDatabase.java
+++ DavidBonelo/pinguinera/src/main/java/com/davidbonelo/PinguDatabase.java
@@ -3,12 +3,14 @@ package com.davidbonelo;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import static com.davidbonelo.Secrets.DB_PASSWORD;
 import static com.davidbonelo.Secrets.DB_URL;
 import static com.davidbonelo.Secrets.DB_USER;
 
 public class PinguDatabase {
+    private static final Logger logger = Logger.getLogger(PinguDatabase.class.getName());
     private static Connection connection;
 
     private static void startConnection() {
@@ -16,7 +18,7 @@ public class PinguDatabase {
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 //            connection.setAutoCommit(false);
         } catch (Exception e) {
-            System.out.println("Database connection error: " + e.getMessage());
+            logger.warning("Database connection error: " + e.getMessage());
         }
     }
 
