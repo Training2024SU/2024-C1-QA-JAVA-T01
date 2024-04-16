74bc119e984628a9f3a1cd9077b331c3ecce3deb
VasquezJulioe <vasquezjulioe@gmail.com>
2024-04-16 17:53:44 -0500

    Se agrega contenido a la documentacion

--- BibliotecaLaPinguinera/src/main/java/com/sofka/BibliotecaLaPinguinera.java
+++ BibliotecaLaPinguinera/src/main/java/com/sofka/BibliotecaLaPinguinera.java
@@ -2,20 +2,22 @@ package com.sofka;
 
 import com.sofka.integration.database.mysql.MySqlOperation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import java.sql.SQLException;
+import java.util.logging.Logger;
 
 import static com.sofka.controllers.ControlMenu.preguntarSalir;
 import static com.sofka.controllers.ControlStament.closeConnection;
 import static com.sofka.controllers.ControlStament.openConnection;
 import static com.sofka.menu.menuDeInicio.menuInicio;
-
 @SpringBootApplication
 public class BibliotecaLaPinguinera {
+private static final Logger logger = Logger.getLogger(BibliotecaLaPinguinera.class.getName());
     public static final String SELECCIONE_CORRECTAMENTE = "Opción incorrecta, por favor seleccione correctamente";
     public static MySqlOperation mySqlOperation = new MySqlOperation();
 
     public static void main(String[] args) throws SQLException, NumberFormatException, NullPointerException {
-        System.out.println("Bienvenido a la Biblioteca la Pingüinera\n");
+        logger.warning("Bienvenido a la Biblioteca la Pingüinera\n");
         do {
             openConnection();
             menuInicio();
