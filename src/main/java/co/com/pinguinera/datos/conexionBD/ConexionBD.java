b0fbe37745edb935c864d4f139a445428e900b7e
DanielMorales <dm34mg@gmail.com>
2024-04-16 18:51:38 -0500

    Configuracion variables de entorno

--- src/main/java/co/com/pinguinera/datos/conexionBD/ConexionBD.java
+++ src/main/java/co/com/pinguinera/datos/conexionBD/ConexionBD.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionBD {
-    // Variables para la conexión
-    private static final String URL = "jdbc:mysql://localhost:3306/pinguinera";
-    private static final String USUARIO = "root";
-    private static final String CONTRASEÑA = "#32zvv48dH";
+    // Nombres de las variables de entorno
+    private static final String URL_ENV = "DB_URL";
+    private static final String USUARIO_ENV = "DB_USUARIO";
+    private static final String CONTRASEÑA_ENV = "DB_CONTRASENA";
 
     // Variable para la conexión
     private static Connection conexion;
@@ -19,9 +19,14 @@ public class ConexionBD {
     // Método estático para obtener la conexión única a la base de datos
     public static Connection conectar() throws SQLException {
         if (conexion == null || conexion.isClosed()) {
-            // Establecemos la conexión con los valores directamente especificados
-            conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
+            String url = System.getenv(URL_ENV);
+            String usuario = System.getenv(USUARIO_ENV);
+            String contraseña = System.getenv(CONTRASEÑA_ENV);
+
+            // Establecemos la conexión con los valores obtenidos de las variables de entorno
+            conexion = DriverManager.getConnection(url, usuario, contraseña);
         }
         return conexion;
     }
+
 }
